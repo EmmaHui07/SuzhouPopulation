8c7f068983d91f37345a45d6340517e802526d36
EmmaHui07 <1535582178@qq.com>
2020-04-03 17:28:06 +0800

    图层发布后，动态加载行政区域树菜单

--- src/webapp/commons/js/config.js
+++ src/webapp/commons/js/config.js
@@ -1,1 +1,2 @@
-var g_BasePath = "http://47.100.49.112:8088/fishser/";
+var xianurl = "http://192.168.1.120:8080/test/SuzhouPopulation/src/webapp/commons/resource/xian.json";
+var baseHttp = "http://192.168.1.120:6080/";
--- src/webapp/commons/js/map.js
+++ src/webapp/commons/js/map.js
@@ -1,53 +1,119 @@
-var zhen_url = "http://localhost:6080/arcgis/rest/services/SZ/MapServer/0";   //镇
-var quxian_url = "http://localhost:6080/arcgis/rest/services/SZ/MapServer/1";   //区县
-var url = "http://localhost:6080/arcgis/rest/services/SZ/MapServer"
+var quxian_url = baseHttp + "arcgis/rest/services/SZ/MapServer/0";   //区县
+var zhen_url = baseHttp +  "arcgis/rest/services/SZ/MapServer/1";   //镇
+var url_all = baseHttp + "arcgis/rest/services/SZ/MapServer";
+
 $(function () {
     initMap();
 });
+var quxianList,zhenList;
 
 //初始化地图
 function initMap() {
     require(["esri/Map",
-        "esri/views/MapView",
-        "esri/layers/MapImageLayer",
-        "esri/layers/FeatureLayer",
-        "dojo/domReady!"], function (Map, MapView, MapImageLayer, FeatureLayer) {
-
-        var map = new Map({
-            basemap: "streets"
-        });
-        //加载图层
-        var layer = new MapImageLayer({
-            url: url
-        });
-        //map.add(layer);
-
-        var layer_zhen = new FeatureLayer({
-            url: zhen_url
-        });
-        map.add(layer_zhen);
-
-        var view = new MapView({
-            container: "mapView",
-            map: map,
-            zoom: 9,
-            center: [121.50, 31.40],
-            slider: false,
-            maxZoom: 18,
-            minZoom: 5
-        });
-        view.ui.remove('attribution')//清除底部powered by ESRI
-        view.when(
-            function () {
-                initLayersController();
-            },
-            function (error) {
-                console.log("wrong at view.when()!");
+            "esri/layers/GeoJSONLayer",
+            "esri/layers/MapImageLayer",
+            "esri/views/MapView",
+            "esri/tasks/QueryTask",
+            "esri/tasks/support/Query",
+            "esri/layers/FeatureLayer",
+            "esri/renderers/SimpleRenderer",
+            "dojo/domReady!"],
+        function (Map, GeoJSONLayer, MapImageLayer, MapView, QueryTask, Query) {
+            var map = new Map({
+                basemap: "gray",
             });
-    });
+            var layer_all = new MapImageLayer({
+                url: url_all
+            });
+            map.add(layer_all);
 
+            const view = new MapView({
+                container: "mapView",
+                map: map,
+                zoom: 9,
+                center: [121.10, 31.40]
+            });
+            view.ui.remove('attribution');//清除底部powered by ESRI
+
+            var Ob_district = [];
+            //区县查询
+            var queryTask_quxian = new QueryTask({
+                url: quxian_url
+            });
+            var query_quxian = new Query({
+                returnGeometry: true,
+                outFields: ["*"]
+            });
+            query_quxian.where = "shi = '苏州市'";
+            queryTask_quxian.execute(query_quxian).then(function (results) {
+                quxianList = results.features;
+
+                //街镇查询
+                var queryTask_zhen = new QueryTask({
+                    url: zhen_url
+                });
+                var query_zhen = new Query({
+                    returnGeometry: true,
+                    outFields: ["*"]
+                });
+                query_zhen.where = "shi = '苏州市'";
+                queryTask_zhen.execute(query_zhen).then(function (results) {
+                    zhenList = results.features;
+                    queryTown(quxianList,zhenList);
+                });
+            });
+
+            function queryTown(quxianList,zhenList) {
+                //获得所有区县名称集合
+                for (var i = 0; i < quxianList.length; i++) {
+                    var quname = quxianList[i].attributes.NAME;
+                    var object = {};
+                    object["DistrictName"] = quname;
+                    var zhens = [];
+                    for (var j = 0; j < zhenList.length; j++) {
+                        if(zhenList[j].attributes.xian == quname){
+                            zhens.push(zhenList[j]);
+                        }
+                    }
+                    object["Districtzhen"] = zhens;
+                    Ob_district.push(object);
+                }
+
+                if (Ob_district != []) {
+                    initLayers(Ob_district);
+                }
+            }
+
+        });
 }
 
-function initLayersController() {
+
+function initLayers(Ob_district) {
+    var str = "";
+    //加区县 & 镇
+    for(var i=0;i<Ob_district.length;i++){
+        if(Ob_district[i].Districtzhen.length > 1){
+            var str1="";
+            for(var j=0;j < Ob_district[i].Districtzhen.length;j++){
+                str1 += "<li><a href='#0'>"+ Ob_district[i].Districtzhen[j].attributes.NAME + "</a></li>"
+            }
+            str += " <li class='has-children'>" +
+                "<input type='checkbox' name ='sub-group-"+ i +"' id='sub-group-"+ i +"' >" +
+                "<label for='sub-group-"+ i +"'>" + Ob_district[i].DistrictName + "</label>" +
+                "<ul>" + str1 +
+                "</ul>" +
+                "</li>";
+        }else {
+            str += "<li><label>" + Ob_district[i].DistrictName + "</label></li>";
+        }
+    }
+
+
+    //仅加区县
+    // for(var i=0;i<Ob_district.length;i++){
+    //     str += "<li><label>" + Ob_district[i].DistrictName + "</label></li>";
+    // }
+
+    $("#qulist").append(str);
 
 }
